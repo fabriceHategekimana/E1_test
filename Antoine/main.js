a0178758561ff416ddf0e2b61b6059caaa4be524
brenozinho <antoine.breno@gmail.com>
2025-02-06 11:48:37 +0100

    js/html

---
+++ Antoine/main.js
@@ -0,0 +1,14 @@
+let a = prompt("Vous allez bien ? ( oui | non )");
+            if (a === "oui") {
+                alert("Parfait")
+                console.log(alert)
+            } else if (a === "non") {
+                alert("cheh")
+                console.log(alert)
+            } else {
+                alert("Je ne suis pas sur d'avoir compris")
+                console.log(alert)
+            }
+
+
+            document.getElementById('demo').innerHTML = "Javascript";
